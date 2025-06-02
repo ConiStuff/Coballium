ea247539d95a95ac862579c3fcbdbfb73db3e4a4
coni <conishadoww@gmail.com>
2025-06-02 13:27:13 -0400

    basic ftp starter spaghetti

---
+++ CoballiumLib/Ftp/Client/FtpClient.vb
@@ -0,0 +1,14 @@
+﻿Imports Windows.Networking
+Imports Windows.Networking.Sockets
+Imports Windows.Storage.Streams
+Imports System.Text
+
+Namespace Coballium.Ftp.Client
+
+    Public Class FtpClient
+        Private socket As StreamSocket
+        Private writer As DataWriter
+        Private reader As DataReader
+        Private host As String
+    End Class
+End Namespace
